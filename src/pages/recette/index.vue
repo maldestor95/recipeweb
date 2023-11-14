f9f0ad6def8b8aa7fbee4f957972ab3768b605bb
maldestor95 <maldestor95@gmail.com>
2023-11-14 07:04:08 +0100

    recette:  menu btn creation

--- src/pages/recette/index.vue
+++ src/pages/recette/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="grid grid-cols-[200px_1fr]">
-    <recipeListForm
-      :recipe-list="recipeList"
-      @choice="(e) => changerecipe(e)"
-    ></recipeListForm>
+  <!-- <div class="grid grid-cols-[200px_1fr]"> -->
+  <div class="flex bg-red-100 items-start absolute top-3 mx-auto">
+    <menubtn>
+      <recipeListForm
+        :recipe-list="recipeList"
+        @choice="(e) => changerecipe(e)"
+      ></recipeListForm>
+    </menubtn>
     <recipeView
       :title="recipechosen"
       :recipeDetails="recipeDetails"
@@ -17,6 +20,7 @@ import { onMounted, ref } from "vue";
 import methods from "./methods";
 import recipeListForm from "./recipelist.vue";
 import recipeView from "./recipeview.vue";
+import menubtn from "../../components/menubtn.vue";
 
 const recipeList = ref([{ title: "testa", link: "linktest" }]);
 const recipechosen = ref("sample recipe chosen");
