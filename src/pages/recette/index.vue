54a0421b27fb8adc647528c5346c034388a7fd41
maldestor95 <maldestor95@gmail.com>
2023-11-22 23:36:49 +0100

    add introduction and redirect to main server

--- src/pages/recette/index.vue
+++ src/pages/recette/index.vue
@@ -1,38 +1,48 @@
 <template>
   <div class="container mx-auto">
-    <div @click="hidemenu = !hidemenu" class="absolute left-0 top-1 w-8 h-8 p-0">
-      <!-- <svg
+    <div @click="menu = 'choice'" class="absolute left-0 top-1 w-8 h-8 p-0">
+      <img src="/public/logo.svg" />
+    </div>
+    <div
+      @click="menu = 'introduction'"
+      v-if="menu == 'choice'"
+      class="absolute right-0 top-1 w-8 h-8 p-0"
+    >
+      <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
-        viewBox="0 0 30 24"
-        stroke-width="3"
+        stroke-width="1.5"
         stroke="currentColor"
-        class="w-6 h-6 mx-auto my-auto"
+        viewBox="0 0 24 24"
+        class="w-8 h-8"
       >
         <path
           stroke-linecap="round"
           stroke-linejoin="round"
-          d="M4 4h22M4 12h22M4 20h22"
+          d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
         />
-      </svg> -->
-      <img src="/public/logo.svg" />
+      </svg>
     </div>
     <recipeListForm
       class="mt-1 px-2"
-      v-if="hidemenu == false"
+      v-if="menu == 'choice'"
       :recipe-list="recipeList"
       @choice="
         (e) => {
           changerecipe(e);
-          hidemenu = true;
+          menu = 'chosen';
         }
       "
     ></recipeListForm>
+
     <recipeView
       :title="recipechosen"
       :recipeDetails="recipeDetails"
       class="mx-auto px-2"
+      v-if="menu == 'chosen'"
     ></recipeView>
+
+    <introduction v-if="menu == 'introduction'"></introduction>
   </div>
 </template>
 
@@ -41,12 +51,13 @@ import { onMounted, ref } from "vue";
 import methods from "./methods";
 import recipeListForm from "./recipelist.vue";
 import recipeView from "./recipeview.vue";
+import introduction from "./introduction.vue";
 // import menubtn from "../../components/menubtn.vue";
 
 const recipeList = ref([{ title: "testa", link: "linktest" }]);
 const recipechosen = ref("sample recipe chosen");
 const recipeDetails = ref(`# sample details`);
-const hidemenu = ref(true);
+const menu = ref("introduction");
 
 onMounted(async () => {
   recipeList.value = await methods.fetchList();
